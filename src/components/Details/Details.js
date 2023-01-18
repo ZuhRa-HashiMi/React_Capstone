0eeaa10550d73bf287720ad426c51b1f6ddfdc3a
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-01-18 12:59:44 +0430

    import files

---
+++ src/components/Details/Details.js
@@ -0,0 +1,7 @@
+import numberSeparator from 'number-separator';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import useCountryData from '../../utils/useCountryData';
+
+import BasicLayoutContainer from '../Layouts/BasicLayout';
