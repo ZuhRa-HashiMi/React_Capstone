da81412fd03f843235ee529fd05deb5f44f2b769
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-01-18 14:16:25 +0430

    add useCounterDate function

---
+++ src/utils/useCountryData.js
@@ -0,0 +1,18 @@
+/* eslint-disable linebreak-style */
+import { useEffect, useState } from 'react';
+
+const useCountryData = (id, countries) => {
+  const [country, setCountry] = useState(null);
+  useEffect(() => {
+    const country = countries.find((c) => c.ID === id);
+    if (country) {
+      setCountry({
+        ...country,
+        flag: `https://countryflagsapi.com/png/${country.CountryCode}`,
+      });
+    }
+  }, [countries]);
+  return country;
+};
+
+export default useCountryData;
